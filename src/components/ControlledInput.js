08e15d8620960bf94ec0bf230a699cbc54bbf3c7
Alexander Thibodeaux <alyxanderthibodeaux@gmail.com>
2018-06-01 18:51:55 +0000

    automatically backed up by learn

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
 class ControlledInput extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      value: '',
+    };
+  }
   
+  handleChange = event => {
+    this.setStae({
+      value: event.target.value,
+    });
+  }
+  
+  render () {
+    return (
+      <input type="text" value={this.state.value} onChange={this.handleChange} />
+    );
+  }
 }
+
+export default ControlledInput;
